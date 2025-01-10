de12bafdf485b1d07ff8235aafaa8aeada75543c
Kitonick <myagkov.dv@gmail.com>
2025-01-10 12:31:53 +0100

    enabling Hyprland

--- configuration.nix
+++ configuration.nix
@@ -75,6 +75,8 @@
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  programs.hyprland.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
